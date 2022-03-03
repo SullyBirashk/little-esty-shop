3bd0d174f4ea21d2aa95a8972b3e2ff4e0b84476
Sully Birashk <sully.birashk@gmail.com>
2022-03-03 14:58:22 -0800

    Created controller method for show page

--- app/controllers/merchant_bulk_discounts_controller.rb
+++ app/controllers/merchant_bulk_discounts_controller.rb
@@ -3,4 +3,8 @@ class MerchantBulkDiscountsController < ApplicationController
   def index
     @bulk_discounts = Merchant.find(params[:merchant_id]).bulk_discounts
   end
+
+  def show
+
+  end
 end
