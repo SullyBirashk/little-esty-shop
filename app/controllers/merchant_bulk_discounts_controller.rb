edf802d6b56433bee526e4f7643e58f141bec0b1
Sully Birashk <sully.birashk@gmail.com>
2022-03-04 15:27:50 -0800

    Called API and assigned it a variable in controller

--- app/controllers/merchant_bulk_discounts_controller.rb
+++ app/controllers/merchant_bulk_discounts_controller.rb
@@ -1,6 +1,7 @@
 class MerchantBulkDiscountsController < ApplicationController
 
   def index
+    @holiday_names = UpcomingHolidaysFacade.find_holidays
     @bulk_discounts = Merchant.find(params[:merchant_id]).bulk_discounts
   end
 
