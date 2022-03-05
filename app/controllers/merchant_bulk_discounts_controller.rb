2b16595390a56a67844490356d28ed4493afae75
Sully Birashk <sully.birashk@gmail.com>
2022-03-04 23:03:52 -0800

    Created Controller Methods

--- app/controllers/merchant_bulk_discounts_controller.rb
+++ app/controllers/merchant_bulk_discounts_controller.rb
@@ -6,6 +6,24 @@ class MerchantBulkDiscountsController < ApplicationController
   end
 
   def show
-
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = @merchant.bulk_discounts.create()
+  end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    merchant.bulk_discounts.create(bulk_discount_params)
+    redirect_to "/merchants/#{merchant.id}/bulk_discounts"
+  end
+
+  private
+
+  def bulk_discount_params
+    params.permit(:percentage, :quantity_threshold)
+  end
+
+
 end
