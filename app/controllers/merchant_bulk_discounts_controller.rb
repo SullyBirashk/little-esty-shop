2e75817a685fc065ebec158e6e5f4621f5e38234
Sully Birashk <sully.birashk@gmail.com>
2022-03-05 22:43:55 -0800

    Update Controller

--- app/controllers/merchant_bulk_discounts_controller.rb
+++ app/controllers/merchant_bulk_discounts_controller.rb
@@ -7,6 +7,7 @@ class MerchantBulkDiscountsController < ApplicationController
   end
 
   def show
+    @merchant = Merchant.find(params[:merchant_id])
     @bulk_discount = BulkDiscount.find(params[:id])
   end
 
@@ -26,6 +27,18 @@ class MerchantBulkDiscountsController < ApplicationController
     bulk_discount = BulkDiscount.find(params[:id])
     bulk_discount.destroy
     redirect_to "/merchants/#{merchant.id}/bulk_discounts"
+  end
+
+  def edit
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:merchant_id])
+    bulk_discount = BulkDiscount.find(params[:id])
+    bulk_discount.update(bulk_discount_params)
+    redirect_to "/merchants/#{@merchant.id}/bulk_discounts/#{bulk_discount.id}"
   end
 
   private
