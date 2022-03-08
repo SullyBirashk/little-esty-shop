0247c11d569c44dc2ceb8ce948e43e0abb5f1227
Sully Birashk <sully.birashk@gmail.com>
2022-03-07 20:51:36 -0800

    add relationships

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,7 +3,8 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
-
+  has_many :merchants, through: :items
+  has_many :bulk_discounts, through: :merchants
   enum status: {"cancelled" => 0, "in progress" => 1, "completed" => 2}
 
   validates_presence_of :status
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,7 +1,9 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
-  
+  has_many :merchants, through: :item
+  has_many :bulk_discounts, through: :merchants
+
   enum status: {"pending" => 0, "packaged" => 1, "shipped" => 2}
 
   validates_presence_of :quantity
